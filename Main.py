7e1affdab33ffc45c611908aa13dae1fa14fd1ac
KrygTomasz <yaami93@gmail.com>
2016-12-08 19:43:32 +0100

    Time counter
    Added time of program execution for better comparision of results.

--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
 from GeneticAlgorithm import GeneticAlgorithm
+import time
 
+startTime = time.time()
 
 def function(x, y):
     # funkcja Rosenbrocka: (1-x)*(1-x) + 100*(y-x*x)*(y-x*x)
     return (1-x)*(1-x) + 100*(y-x*x)*(y-x*x)
 
 numberOfPopulationMembers = 1000
-percentOfBestOnesToLive = 0.2
+percentOfBestOnesToLive = 0.8
 searchingSection = [-100, 100]
 
 GA = GeneticAlgorithm(numberOfPopulationMembers, percentOfBestOnesToLive, searchingSection, function)
@@ -16,3 +18,4 @@ print "Searching approximated minimum..."
 minimumValue = GA.searchMinimum(iterations=1000)
 minimumPoint = GA.getArgumentsOfMinimumValue()
 print "Found minimum ", minimumValue, " at point ", minimumPoint
+print("Searching time: %s seconds." % (time.time() - startTime))
